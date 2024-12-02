f79219541db166cc82dd6e611678ac0862dd2614
Troy Washington <troyvw96@gmail.com>
2024-12-01 20:22:44 -0800

    submission

---
+++ src/ItemsList.js
@@ -0,0 +1,52 @@
+const ItemsList = [
+    {
+        id: 1,
+        name: "Mac mini (Apple M4 chip)",
+        details: {
+            category: "electronics",
+            notes: "Apple M4 chip with 10-core CPU, 10-core GPU, 16-core Neural Engine"
+        },
+        images: [
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-mini-202410-gallery-1?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1728265561587" },
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-mini-202410-gallery-2?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1728265561633" },
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-mini-202410-gallery-3?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1730870207690" },
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-mini-202410-gallery-4_GEO_US?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1731027963236" }
+        ]
+    },
+    {
+        id: 2,
+        name: "Mac Studio",
+
+        details: {
+            category: "electronics",
+            notes: "Apple M2 Max with 12‑core CPU, 30‑core GPU, 16‑core Neural Engine"
+        },
+        images: [
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-studio-202306-gallery-1?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1683939204737" },
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-studio-202306-gallery-2?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1683939204939" },
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-studio-202306-gallery-3?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1683939204685" },
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-studio-202306-gallery-4?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1683939207761" },
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-studio-202306-gallery-5?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1683939203708" },
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-studio-202306-gallery-6?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1683939212887" }
+        ]
+    },
+    {
+        id: 3,
+        name: "MacBook Pro 16-inch",
+        details: {
+            category: "electronics",
+            notes: "16-inch Liquid Retina XDR display²"
+        },
+        images: [
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16-spaceblack-gallery1-202410?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1729266976923" },
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16-spaceblack-gallery2-202410_GEO_US?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1729289041704" },
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16-spaceblack-gallery3-202410?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1729266962714" },
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16-spaceblack-gallery4-202410?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1729266972019" },
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16-spaceblack-gallery5-202410?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1729266967320" },
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16-spaceblack-gallery6-202410?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1729266978025" },
+            { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16-spaceblack-gallery7-202410?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=1731003239290" }
+        ]
+    },
+]
+
+export default ItemsList;
--- src/ShoppingListWithImmer.js
+++ src/ShoppingListWithImmer.js
@@ -1,29 +1,167 @@
-import React from 'react';
-import { useImmer } from "immer";
+import { React, useState } from 'react';
+import { useImmer } from "use-immer";
+import { produce } from 'immer';
+import "./styles.sass";
+import ItemsList from "./ItemsList";
 
 function ShoppingListWithImmer() {
-    const [shoppingList, setShoppingList] = useImmer({
-        id: "",
+    const [shoppingList, setShoppingList] = useImmer([{
+        id: 0,
         name: "",
-        quantity: "",
+        quantity: 0,
         details: {
             category: "",
             notes: ""
-        }
-    })
+        },
+        url: ""
+    }])
 
-    const addItem = () => {
+    const addItem = (item) => {
+        setShoppingList((draft) => {
+            const existingItem = draft.find((cartItem) => cartItem.id === item.id);
+            if (existingItem) {
+                existingItem.quantity += 1;
+            } else {
+                draft.push({
+                    ...item,
+                    quantity: 1,
+                    url: item.images[0]?.url || ""
+                });
+            }
+        });
+    };
 
-    }
-    const updateItem = () => {
+    const updateItem = (id, action) => {
+        setShoppingList((draft) => {
+            const item = draft.find((cartItem) => cartItem.id === id);
+            if (item) {
+                if (action === 'increment') {
+                    item.quantity += 1; // Increment quantity
+                } else if (action === 'decrement' && item.quantity > 0) {
+                    item.quantity -= 1; // Decrement quantity, ensuring it doesn't go below 0
+                }
+            }
+        });
+    };
 
-    }
-    const removeItem = () => {
 
-    }
+    const removeItem = (id) => {
+        setShoppingList((draft) => {
+            return draft.filter((item) => item.id !== id);
+        });
+    };
+
+
+    const [curr, setCurr] = useState({});
+
+    const prev = (item, id) => {
+        setCurr((prevCurr) => {
+            const newCurr = { ...prevCurr };
+            if (item.id === id) {
+                const currentIndex = newCurr[id] || 0;
+                newCurr[id] = (currentIndex === 0 ? item.images.length - 1 : currentIndex - 1);
+                console.log("New curr:", newCurr); // Debugging the updated curr
+            }
+            return newCurr;
+        });
+    };
+
+    const next = (item, id) => {
+        // Check if the item has an entry in the curr object
+        setCurr((prevCurr) => {
+            const newCurr = { ...prevCurr };
+            if (item.id === id) {
+                const currentIndex = newCurr[id] || 0; // Default to 0 if no value found for this item
+                newCurr[id] = (currentIndex === item.images.length - 1 ? 0 : currentIndex + 1);
+                console.log("New curr:", newCurr); // Debugging the updated curr
+            }
+            return newCurr;
+        });
+    };
 
     return (
-        <div>ShoppingListWithImmer</div>
+        <div className='layout'>
+            <p>layout</p>
+            {/* // Shopping Cart */}
+            <div className='section'>
+                <p>section</p>
+                <div className='shoppingCart'>
+                    <h2>Shopping Cart</h2>
+                    <h3>cart</h3>
+                    <ul className='shoppingCart_items'>
+                        <p>shoppingCart_items</p>
+                        {shoppingList.map((draft) => (
+                            (draft.name && draft.details.category && draft.url && draft.details.notes && draft.quantity > 0) ? (
+                                <div key={draft.id} className='item'>
+                                    <p>item</p>
+                                    <strong>{draft.name}</strong>
+                                    <img src={draft.url} width={100} />
+                                    <div className='details'>
+                                        <p>Quantity:{draft.quantity}</p>
+                                        <button id='removeItem' onClick={() => removeItem(draft.id)
+                                        }>
+                                            remove
+                                        </button>
+                                        <div className='updateItem'>
+                                            <button onClick={() => updateItem(draft.id, 'decrement')}>-</button>
+                                            <button onClick={() => updateItem(draft.id, 'increment')}>+</button>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            ) : null
+                        ))}
+                    </ul>
+                </div>
+
+            </div>
+
+
+            <div className='section'>
+                <p>section</p>
+                {/* Item list */}
+                <h2>itemList</h2>
+                <ul className='itemList'>
+                    {ItemsList.map((item, index) => (
+                        <div className='item_container'>
+                            <p>item_container</p>
+                            <strong>{item.name}</strong>
+                            <div key={item.id} className='item'>
+                                <div className='slideshow_navigation'>
+                                    <button onClick={() => prev(
+                                        item,
+                                        item.id
+                                    )}>
+                                        <h1>&lt;</h1>
+                                    </button>
+                                </div>
+                                <div className='slideshow'>
+                                    <div className='slideshow_slide' style={{ transform: `translateX(-${(curr[item.id] || 0) * 100}%)` }}>
+                                        {/* Image slides */}
+                                        {item.images.map((image, index) => (
+                                            <img src={image.url} alt='Item' className='slideshow_image'
+                                                key={index} />
+                                        ))}
+                                    </div>
+                                </div>
+                                <div className='slideshow_navigation'>
+                                    <button onClick={() => next(
+                                        item,
+                                        item.id
+                                    )}>
+                                        <h1>&gt;</h1>
+                                    </button>
+                                </div>
+                            </div>
+                            <button id='item' onClick={() => addItem(item)}>
+                                Add to Cart
+                            </button>
+                        </div>
+                    ))
+                    }
+                </ul >
+            </div >
+        </div >
     )
 }
 
